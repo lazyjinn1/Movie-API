3bd6ec0a750420bae87ab3bbb7dadc043daba2ad
lazyjinn1 <joshjbarba@gmail.com>
2023-10-30 14:23:16 -0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,45 +1,38 @@
-//this has to be the same key used in the JWTStrategy
-const jwtSecret = 'your_jwt_secret'; 
-const jwt = require('jsonwebtoken');
-const passport = require('passport');
-//the local passport file.
-require('./passport'); 
+const jwtSecret = 'your_jwt_secret'; // This has to be the same key used in the JWTStrategy
 
-let generateJWTToken = (user) => {
-    return jwt.sign(user, jwtSecret, {
-        //This is the username you are encoding in the JWT
-        subject: user.Username, 
-        //This specifies that the token will expire in 7 days.
-        expiresIn: '7d',
-        //This is the algorithm used to 'sign' or encode the values of the JWT 
-        algorithm: 'HS256' 
-    });
-}
+const jwt = require('jsonwebtoken'),
+    passport = require('passport');
 
-// POST Login
-module.exports = (router) => {
-    router.post('/login', (request, response) => {
-        // this tries to either give or deny authentication to user
-        passport.authenticate('local', { session: false }, (error, user, info) => {
-            // if there is an error or user is not found, this is what is sent:
+    require('./passport'); // Your local passport file
+
+    let generateJWTToken = (user) => {
+        return jwt.sign(user, jwtSecret, {
+      subject: user.Username,  // This is the username you’re encoding in the JWT
+          expiresIn: '7d', // This specifies that the token will expire in 7 days
+          algorithm: 'HS256' // This is the algorithm used to “sign” or encode the values of the JWT
+        });
+      }
+      
+      
+      //Login
+      module.exports = (router) => {
+        router.post('/login', (req, res) => {
+          passport.authenticate('local', { session: false }, (error, user, info) => {
+          
             if (error || !user) {
-                return response.status(400).json({
-                    message: 'Something is not right.',
-                    user:user
-                });
+              return res.status(400).json({
+                message: 'Something is not right',
+                user: user
+              });
             }
-            // if the user exists and there is not currently a session going, then this goes through
-            request.login(user, { session: false }, (error) => {
-                // if an error occurs then this triggers
-                if (error) {
-                    response.send(error);
-                }
-                // this generates the key that allows the user to enter privated endpoints
-                let token = generateJWTToken(user.toJSON());
-                return response.json({user, token}); 
+            
+            req.login(user, { session: false }, (error) => {
+              if (error) {
+                res.send(error);
+              }
+              let token = generateJWTToken(user.toJSON());
+              return res.json({ user, token });
             });
-        }) (request, response);
-    });
-}
-
-
+          })(req, res);
+        });
+      }
